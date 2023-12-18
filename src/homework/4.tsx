8aff75e041e3cab93acd3ce34e3f90c83694e589
nikitkachan <nikyc777@gmail.com>
2023-12-18 09:23:51 +0200

    готово

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -12,11 +12,11 @@ type Menu = { id: MenuIds; title: string };
 
 // Додати тип Menu Selected
 type SelectedMenu = {
-  id?: MenuIds;
+  id: MenuIds;
 };
 
 const MenuSelectedContext = createContext<{ selectedMenu: SelectedMenu }>({
-  selectedMenu: {},
+  selectedMenu: { id: "first" },
 });
 
 // Додайте тип MenuAction
@@ -34,7 +34,9 @@ type PropsProvider = {
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({
+    id: "first",
+  });
 
   const menuContextAction = useMemo(
     () => ({
